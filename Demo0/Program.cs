1444f673bfa7027698850b6176e3350f96c744a7
Michiel Doeven <m.doeven@sharevalue.nl>
2021-01-29 21:48:15 +0100

    Demo 0: Plain Old Class ;)

---
+++ Demo0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Creating instances from Developer class
+
+var developer = new Developer()
+{
+	Company = "Sharevalue",
+	Profession = ".NET Developer",
+	Name = "Michiel"
+};
+
+var newdeveloper = new Developer()
+{
+	Company = developer.Company,
+	Profession = developer.Profession,
+	Name = "Jasper"
+};
+
+Console.WriteLine(DeveloperString(developer));
+Console.WriteLine(DeveloperString(newdeveloper));
+
+Console.ReadLine();
+
+static string DeveloperString(Developer dev)
+{
+	return $"Name: {dev.Name}, Company: {dev.Company}, Profession: {dev.Profession}";
+}
