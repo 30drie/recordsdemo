d2b969028f7d27fe9f4a34567cd0c05667941821
Michiel Doeven <m.doeven@sharevalue.nl>
2021-02-01 21:44:39 +0100

    Demo 4

---
+++ Demo4/Developer.cs
@@ -0,0 +1,9 @@
+﻿public record Developer
+{
+	public string Company { get; init; }
+	public string Profession { get; init; }
+	public string Name { get; init; }
+
+	public Developer(string name, string comp, string prof) => 
+		(Company, Profession, Name) = (comp, prof, name);
+}
---
+++ Demo4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// Creating instances from Developer record with constructor
+
+var developer = new Developer("Michiel", "ShareValue", ".NET Developer");
+
+Console.WriteLine(DeveloperString(developer));
+
+Console.ReadLine();
+
+static string DeveloperString(Developer dev)
+{
+	return $"Name: {dev.Name}, Company: {dev.Company}, Profession: {dev.Profession}";
+}
