bf18c74c03c7b11402a3a38a79a43327d33e521a
Michiel Doeven <m.doeven@sharevalue.nl>
2021-02-01 23:21:02 +0100

    Demo 6

---
+++ Demo6/Developer.cs
@@ -0,0 +1,20 @@
+﻿public record Developer
+{
+	public string Company { get; init; }
+	public string Profession { get; init; }
+	public string Name { get; init; }
+
+	public Developer(string name, string comp, string prof) => 
+		(Company, Profession, Name) = (comp, prof, name);
+
+	public void Deconstruct(out string name, out string comp, out string prof) =>
+		(comp, prof, name) = (Company, Profession, Name);
+}
+
+public record CompetenceLead : Developer
+{
+	public string Competence { get; init; }
+
+	public CompetenceLead(string name, string company, string prof, string competence) : base(name, company, prof) => Competence = competence;
+
+}
---
+++ Demo6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Creating instances from inherited record
+
+var developer = new Developer("Michiel", "ShareValue", ".NET Developer");
+
+var cl = new CompetenceLead(developer.Name, developer.Company, developer.Profession, ".NET");
+
+Console.WriteLine(DeveloperString(developer));
+Console.WriteLine(DeveloperString(cl));
+
+Console.WriteLine($"developer == cl : {developer == cl}");
+Console.WriteLine($"developer == (Developer)cl : {developer == (Developer)cl}");
+
+Console.ReadLine();
+
+static string DeveloperString(Developer dev)
+{
+	return $"HashCode: {dev.GetHashCode()}, Name: {dev.Name}, Company: {dev.Company}, Profession: {dev.Profession}";
+}
