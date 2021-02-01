c4e19651b02df57926829e9f9369f5e79f657f54
Michiel Doeven <m.doeven@sharevalue.nl>
2021-02-01 22:55:43 +0100

    Demo 5

---
+++ Demo5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Creating instances from Developer record with short syntax
+
+var developer = new Developer("Michiel", "ShareValue", ".NET Developer");
+
+Console.WriteLine(DeveloperString(developer));
+
+developer.Deconstruct(out string comp, out string prof, out string name);
+Console.WriteLine($"Name: {name}, Company: {comp}, Profession: {prof}");
+
+Console.ReadLine();
+
+static string DeveloperString(Developer dev)
+{
+	return $"Name: {dev.Name}, Company: {dev.Company}, Profession: {dev.Profession}";
+}
+
+//How short?
+public record Developer(string Company, string Profession, string Name);
