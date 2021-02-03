3b9eb60275a4e4893bdad1e6ed145a6b470d7814
Michiel Doeven <m.doeven@sharevalue.nl>
2021-02-03 22:09:28 +0100

    Demo 7 (incorrectly use of records)

---
+++ Demo7/Developer.cs
@@ -0,0 +1,6 @@
+﻿public record Developer
+{
+	public string Company { get; set; }
+	public string Profession { get; set; }
+	public string Name { get; set; }
+}
---
+++ Demo7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Creating instances from Developer record
+
+var developer = new Developer()
+{
+	Company = "Sharevalue",
+	Profession = ".NET Developer",
+	Name = "Michiel"
+};
+
+var newdeveloper = developer with { Name = "Jasper" };
+newdeveloper.Profession = "Competence Lead";
+
+Console.WriteLine(DeveloperString(developer));
+Console.WriteLine(DeveloperString(newdeveloper));
+
+Console.ReadLine();
+
+static string DeveloperString(Developer dev)
+{
+	return $"HashCode: {dev.GetHashCode()}, Name: {dev.Name}, Company: {dev.Company}, Profession: {dev.Profession}";
+}
